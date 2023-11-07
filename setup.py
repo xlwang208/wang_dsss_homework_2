05f996a9eea78e03f0fc75628806eff2fd622011
Xiaoliang Wang <wcarina0208@gmail.com>
2023-11-07 18:56:40 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='math-quiz-package',
+    version='1.0.0',
+    description='A math quiz game package',
+    author='Xiaoliang Wang',
+    author_email='wcarina0208@gmail.com',
+    packages=['math_quiz_package'],
+)
